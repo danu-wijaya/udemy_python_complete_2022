281e227b4499a2d855734925c9dcb573eb4d4b2e
Kamandanu Tresna Wijaya <kamandanu.wijaya@gmail.com>
2022-12-27 22:14:39 +0700

    41_zip_function

---
+++ 2_Fundamentals/41_zip_function.py
@@ -0,0 +1,6 @@
+old_friends = ["Rolf", "Bob", "Jen", "Anne"]
+time_since_seen = [3, 7, 15, 11]
+
+long_timer = dict(zip(old_friends, time_since_seen))
+
+print(long_timer)
---
+++ 2_Fundamentals/42_enumerete_function.py
@@ -0,0 +1,8 @@
+friends = ["rolf", "John", "Anna"]
+
+for counter, friend in enumerate(friends, start=1):
+    print(counter)
+    print(friend)
+    
+print(list(enumerate(friends)))
+print(dict(enumerate(friends, start=1)))
